bdfc91c832c6d0a841feb0a498abbd34d111c37c
vragon <drav.nguyenngocanh@gmail.com>
2025-02-08 04:24:24 +0700

    Project complete

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,12 @@ app.use(express.json());
 
 app.use("/api/products",productRoutes); 
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+}
 app.listen(PORT, () => {
     connectDB();
     console.log('Server started at http://localhost:' + PORT);
