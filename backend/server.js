2a8e1598883d239f50786aa7cad118edf49ea295
vragon <drav.nguyenngocanh@gmail.com>
2025-02-08 05:02:02 +0700

    Fix bug

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 
 dotenv.config();
-const MONGOOSE = "mongodb+srv://dbAdmin01:acbrtxoiu89@kj@cluster0.wv6ym.mongodb.net/product?retryWrites=true&w=majority&appName=Cluster0";
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 //Allow to accept JSON data in the req.body
 app.use(express.json());
 
 app.use("/api/products",productRoutes); 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
 app.listen(PORT, () => {
